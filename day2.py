00a4bf9510895f40c5a4f984f46f3d6b0c99a4e8
Marcel Wójcik <marcelw.dev@gmail.com>
2021-12-04 16:57:08 +0100

    Day 2 Solution

---
+++ day2.py
@@ -0,0 +1,43 @@
+# get input
+commands = []
+with open("day2_input.txt") as f:
+    for line in f:
+        line = line.split(" ")
+        commands.append([line[0], int(line[1])])
+
+
+def part_1():
+    horizontal = 0
+    depth = 0
+    for line in commands:
+        [command, value] = line
+        if(command == 'forward'):
+            horizontal += value
+        elif(command == 'down'):
+            depth += value
+        elif(command == 'up'):
+            depth -= value
+            
+    return horizontal * depth
+
+
+def part_2():
+    horizontal = 0
+    depth = 0
+    aim = 0
+
+    for line in commands:
+        [command, value] = line
+        if(command == 'forward'):
+            horizontal += value
+            depth += (aim * value) 
+        elif(command == 'down'):
+            aim += value
+        elif(command == 'up'):
+            aim -= value
+            
+    return horizontal * depth
+
+
+print("Solution Part 1: ", part_1()) # 2147104
+print("Solution Part 1: ", part_2()) # 2044620088
